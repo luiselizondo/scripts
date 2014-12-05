46aa11811619bc3dae2f1f18a519a58356d97ec1
Luis Elizondo <lelizondo@gmail.com>
2014-12-05 13:12:27 -0600

    Instalar pip sin preguntar

--- install-maestro-ng.sh
+++ install-maestro-ng.sh
@@ -2,10 +2,10 @@
 
 echo "Instalando maestro-ng"
 
-apt-get install python-pip python-dev build-essential
+apt-get install -y python-pip python-dev build-essential
 pip install --upgrade pip 
 pip install --upgrade virtualenv 
-apt-get install python-yaml
+apt-get install -y python-yaml
 
 pip install --upgrade git+git://github.com/signalfuse/maestro-ng
 

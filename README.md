ad7e77d2377ae4f381375b3e244fbef35dd0a815
Luis Elizondo <lelizondo@gmail.com>
2014-12-05 13:10:59 -0600

    Agregar instalador para maestro-ng

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Install Docker Registry
+		
+		curl -sSL https://raw.githubusercontent.com/luiselizondo/scripts/master/install-docker-registry.sh | sh
+
+# Install Maestro-ng
+
+		curl -sSL https://raw.githubusercontent.com/luiselizondo/scripts/master/install-maestro-ng.sh | sh
---
+++ install-maestro-ng.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Instalando maestro-ng"
+
+apt-get install python-pip python-dev build-essential
+pip install --upgrade pip 
+pip install --upgrade virtualenv 
+apt-get install python-yaml
+
+pip install --upgrade git+git://github.com/signalfuse/maestro-ng
+
+MAESTRO_LOCATION=$(which maestro)
+
+echo "Maestro está instalado en $MAESTRO_LOCATION"
+

37e4ebb2baf94c55aad2577c75fa74c6b244c303
Luis Elizondo <lelizondo@gmail.com>
2014-12-05 17:00:27 -0600

    Error en install-maestro

--- install-maestro-ng.sh
+++ install-maestro-ng.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo "Instalando maestro-ng"
-
+echo "Actualizando"
 apt-get update
 apt-get install -y python-pip python-dev build-essential python-yaml
-/usr/local/bin/pip install --upgrade pip 
-/usr/local/bin/pip install --upgrade virtualenv 
+/usr/bin/pip install --upgrade pip 
+/usr/bin/pip install --upgrade virtualenv 
 
-/usr/local/bin/pip install --upgrade git+git://github.com/signalfuse/maestro-ng
+echo "Instalando maestro-ng"
+/usr/bin/pip install --upgrade git+git://github.com/signalfuse/maestro-ng
 
 MAESTRO_LOCATION=$(which maestro)
+PRIVATE_IP="$(ifconfig eth1 | awk -F ' *|:' '/inet addr/{print $4}')"
 
 echo "Maestro está instalado en $MAESTRO_LOCATION"
-
+echo "Maestro esta usando la IP interna $PRIVATE_IP"

33f764c3c2eb933fd7c473b9ac94c304fbc085a8
Luis Elizondo <lelizondo@gmail.com>
2014-12-05 17:14:26 -0600

    Error al procesar la IP interna

--- install-maestro-ng.sh
+++ install-maestro-ng.sh
@@ -14,7 +14,7 @@ echo "Instalando maestro-ng"
 $PIP install --upgrade git+git://github.com/signalfuse/maestro-ng
 
 MAESTRO_LOCATION=$(which maestro)
-PRIVATE_IP="$(ifconfig eth1 | awk -F ' *|:' '/inet addr/{print $4}')"
+PRIVATE_IP=$(ifconfig eth1 | awk -F ' *|:' '/inet addr/{print $4}')
 
 echo "Maestro está instalado en $MAESTRO_LOCATION"
 echo "Maestro esta usando la IP interna $PRIVATE_IP"

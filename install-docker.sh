4e87632e274daea735d957544ccac22240ec3dcd
Luis Elizondo <luiselizondo@users.noreply.github.com>
2014-12-15 19:17:11 -0600

    Change port of docker

--- install-docker.sh
+++ install-docker.sh
@@ -10,7 +10,7 @@ die () {
 curl -sSL https://get.docker.com/ubuntu/ | sh
 
 echo "Configuring Docker"
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --dns 8.8.8.8 --dns 8.8.4.4 --insecure-registry docker.iiiepe.net:80"' >> /etc/default/docker
+echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --dns 8.8.8.8 --dns 8.8.4.4 --insecure-registry docker.iiiepe.net:5000"' >> /etc/default/docker
 service docker restart
 
 echo "Configuring IP Tables"

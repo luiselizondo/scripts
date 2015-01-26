0af5308b173ffb1e0719fd7f70ff2a8ca8aed34b
Luis Elizondo <luiselizondo@users.noreply.github.com>
2015-01-26 17:31:48 -0600

    Update install-docker.sh para remover insecure-registry

--- install-docker.sh
+++ install-docker.sh
@@ -12,7 +12,8 @@ die () {
 curl -sSL https://get.docker.com/ubuntu/ | sh
 
 echo "Configuring Docker"
-echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --dns 8.8.8.8 --dns 139.111.0.197 --insecure-registry docker.iiiepe.net:5000"' >> /etc/default/docker
+echo 'DOCKER_OPTS="-H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock --dns 8.8.8.8 --dns 139.111.0.197"' >> /etc/default/docker
+
 service docker restart
 
 echo "Configuring IP Tables"
